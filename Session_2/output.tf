405d86d9d8d075e689864a9a580db660adec7f21
Bayu Adin <modbaybah@gmail.com>
2020-07-08 18:19:20 +0700

    Fixing duplicate output

--- Session_2/output.tf
+++ Session_2/output.tf
@@ -1,43 +1,44 @@
 #### ZONE A #####
 
-output "instance_id" {
+output "instance_id_A" {
   description = "Instance ID"
   value   = module.Instance_ZoneA.this_instance_id
 }
 
-output "instance_public_ip" {
+output "instance_public_ip_A" {
   description = "Instance ID"
   value   = module.Instance_ZoneA.this_instance_public_ip
 }
 
-output "vswitch_id" {
+output "vswitch_id_A" {
   description = "Instance ID"
   value   = module.Instance_ZoneA.this_vswitch_id
 }
 
-output "security_group_id" {
+output "security_group_id_A" {
   description = "Instance ID"
   value   = module.Instance_ZoneA.this_security_group_id
 }
 
 #### ZONE B #####
 
-output "instance_id" {
+output "instance_id_B" {
   description = "Instance ID"
   value   = module.Instance_ZoneB.this_instance_id
 }
 
-output "instance_public_ip" {
+output "instance_public_ip_B" {
   description = "Instance ID"
   value   = module.Instance_ZoneB.this_instance_public_ip
 }
 
-output "vswitch_id" {
+output "vswitch_id_B" {
   description = "Instance ID"
   value   = module.Instance_ZoneB.this_vswitch_id
 }
 
-output "security_group_id" {
+output "security_group_id_B" {
   description = "Instance ID"
   value   = module.Instance_ZoneB.this_security_group_id
 }
+  
